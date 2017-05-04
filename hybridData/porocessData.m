13e1b9a37301f0ca4198f6e8baf35116309a5b90
fjam100 <francisjames10@gmail.com>
2017-05-03 17:00:42 -0700

    a

--- hybridData/porocessData.m
+++ hybridData/porocessData.m
@@ -3,7 +3,7 @@ clear all;
 close all;
 
 % choose file %
-filenameRoot = 'data5_1_5_31pmspindleon5n';
+filenameRoot = 'data4_30_9_42pmspindleon10imp';
 % filename
 % k = 2;      % when the torque is %50 reduced.
 % filename = [filenameRoot,num2str(k)];
